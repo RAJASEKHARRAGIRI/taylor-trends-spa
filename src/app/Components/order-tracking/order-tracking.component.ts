094f4cb0abfcda01316d0034cdcfe139b07a632f
Rajasekhar Ragiri <ragirirajasekhar@gmail.com>
2025-02-25 08:22:06 +0530

    changes in order tracking page

--- src/app/Components/order-tracking/order-tracking.component.ts
+++ src/app/Components/order-tracking/order-tracking.component.ts
@@ -1,12 +1,14 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   standalone: true,
   selector: 'order-tracking',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './order-tracking.component.html',
   styleUrl: './order-tracking.component.css'
 })
 export class OrderTrackingComponent {
-
+  futureDate: Date = new Date();
+  public estimatedDeliveryDate: Date = new Date(this.futureDate.setDate(new Date().getDate() + 5));
 }
